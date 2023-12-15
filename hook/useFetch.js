eb25348ce509ae8093daccf53ff9554a3b55f8dd
Ibrahim Ahmat <ibr.s.ahmat@gmail.com>
2023-12-15 14:46:29 +0300

    Fix an error

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -3,15 +3,14 @@ import axios from 'axios';
 import { RAPID_API_KEY } from '@env';
 
 const rapidApiKey = RAPID_API_KEY;
-
-const useFetch = ({ endPoint }) => {
+const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const options = {
     method: 'GET',
-    url: `https://jsearch.p.rapidapi.com/${endPoint}`,
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
       'X-RapidAPI-Key': rapidApiKey,
       'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
@@ -19,28 +18,29 @@ const useFetch = ({ endPoint }) => {
     params: { ...query },
   };
 
-  const fetData = async () => {
+  const fetchData = async () => {
     setIsLoading(true);
-    try {
-      const res = await axios.request(options);
 
-      setData(res.data.data);
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
       setIsLoading(false);
     } catch (error) {
       setError(error);
-      alert('There is an error!!');
+      console.log(error);
     } finally {
       setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    fetData();
+    fetchData();
   }, []);
 
   const refetch = () => {
     setIsLoading(true);
-    fetData();
+    fetchData();
   };
 
   return { data, isLoading, error, refetch };
